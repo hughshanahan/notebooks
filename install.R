a23ed2030dd892ca6713bf70054cb7c7485f6838
Hugh Shanahan <Hugh.Shanahan@physics.org>
2019-05-26 21:30:59 +0100

    Update install.R
    ghql is on ropensci not on cran  - use devtools to download it

--- install.R
+++ install.R
@@ -1,5 +1,7 @@
 install.packages("httr")
-install.packages("ghql")
+install.packages("devtools")
+library(devtools)
+install_github("ropensci/ghql")
 install.packages("jsonlite")
 install.packages("IRdisplay")
 install.packages("dplyr")
